6ab301a8bdd1c8c60a1d6a5f78b0d2e5619af998
lajbel <lajbelms@gmail.com>
2024-02-15 10:26:07 -0300

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://lajbel.com",
-	integrations: [tailwind(), preact(), icon()],
+  site: "https://lajbel.com",
+  integrations: [tailwind(), preact(), icon(), sitemap()]
 });
